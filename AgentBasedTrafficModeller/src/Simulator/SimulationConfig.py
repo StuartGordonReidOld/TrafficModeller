32fd9b93c810d578440275e2ee06432215e8c9f3
Stuart Reid <stuart@sinkwa.com>
2014-01-08 21:22:48 +0200

    TODOs added to SimulationConfig file in the Simulator folder

--- AgentBasedTrafficModeller/src/Simulator/SimulationConfig.py
+++ AgentBasedTrafficModeller/src/Simulator/SimulationConfig.py
@@ -5,5 +5,38 @@
 __author__="stuart"
 __date__ ="$08 Jan 2014 9:05:50 PM$"
 
+class SimulationConfig:
+    totalVehicles = 0
+    lightVehicles = 0
+    heavyVehicles = 0
+    
+    def __init__(self):
+        self.totalVehicles = 0
+        self.lightVehicles = 0
+        self.heavyVehicles = 0
+
+        return
+    
+    def run(self,fileName):
+        self.readData(fileName)
+        self.printData()
+    
+    def readData(self, fileName):
+        # TODO: Open up the Config text file
+        # TODO: Read values into the 
+        # 1. total Vehicles,
+        # 2. light Vehicles, and
+        # 3. heavy Vehicles variables
+        # TODO: Add additional variables and lines in the Config text file for
+        # additional required information e.g. weights of vehicles? etc.
+        return
+    
+    def printData(self):
+        print "Total vehicles is ", self.totalVehicles
+        print "Heavy vehicles is ", self.heavyVehicles
+        print "Light vehicles is ", self.lightVehicles
+        return
+
 if __name__ == "__main__":
-    print "Hello World"
+    config = SimulationConfig()
+    config.run("Config")
